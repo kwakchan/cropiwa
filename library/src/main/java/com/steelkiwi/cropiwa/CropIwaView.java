543ad8c1841d8117378e6ae557de2a140aaf94fc
xhan <xhan@bubbletap.com>
2024-01-16 09:19:45 +0900

    Feat: 백그라운드 컬러 변경 & IOException에서 Exception으로 변경

--- library/src/main/java/com/steelkiwi/cropiwa/CropIwaView.java
+++ library/src/main/java/com/steelkiwi/cropiwa/CropIwaView.java
@@ -81,7 +81,7 @@ public class CropIwaView extends FrameLayout {
             throw new IllegalStateException("imageConfig must be initialized before calling this method");
         }
         imageView = new CropIwaImageView(getContext(), imageConfig);
-        imageView.setBackgroundColor(Color.BLACK);
+        imageView.setBackgroundColor(Color.WHITE); // 백그라운드 컬러 white로 커스텀
         gestureDetector = imageView.getImageTransformGestureDetector();
         addView(imageView);
     }
